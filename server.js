6bc3de3b89ff5f12224b5088f83bd42fef25035d
hoangquandinh2109 <34148141+hoangquandinh2109@users.noreply.github.com>
2025-08-14 20:05:38 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// API endpoints
+app.get('/api/data1', (req, res) => {
+  res.json({ message: 'This is API 1' });
+});
+
+app.get('/api/data2', (req, res) => {
+  res.json({ message: 'This is API 2' });
+});
+
+// Serve static files (frontend)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch-all for SPA routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
